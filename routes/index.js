4eeeebd3d9becda136a61a99a0ac70abb900f7b6
visaals <visaal.sivakumar@gmail.com>
2019-07-13 19:00:56 -0400

    changed index.html path

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ app.post('/api/bulk-directions', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
