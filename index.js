46fa70505cf0193182b81235ac92d7efb488c86d
visaals <visaal.sivakumar@gmail.com>
2019-07-14 21:15:13 -0400

    moved index out of routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const directionsController = require('../controllers/DirectionsController');
+const directionsController = require('./controllers/DirectionsController');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -23,7 +23,7 @@ app.post('/api/bulk-directions', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
